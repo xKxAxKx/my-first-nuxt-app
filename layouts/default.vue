bf54ef44ba8f0f08e5ebc340b88e4376cea10d88
xKxAxKx <pekepooooong@gmail.com>
2019-01-03 20:22:45 +0900

    レイアウトのルールとdefault.vueの編集

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-    <nuxt/>
+    <span>default layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <hr>
+    <footer>
+      footer
+    </footer>
   </div>
 </template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
 
 <style>
 html {
@@ -50,4 +67,3 @@ html {
   background-color: #35495e;
 }
 </style>
-
